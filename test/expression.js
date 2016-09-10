3df6b78e270de0c4539baef76a6120684dcf85c9
Aymeric Beaumet <hi@aymericbeaumet.com>
2016-09-10 16:40:12 +0200

    style: fix indent

--- test/expression.js
+++ test/expression.js
@@ -2,44 +2,44 @@ import { expand } from '../src/expression'
 
 describe('expression#expand', () => {
 
-    it('should throw if a different type than ExpressionStatement is find in the Program body', () => {
-      expect(expand.bind(null, `
-        const foo = 'bar'
-      `)).toThrowError('Only ExpressionStatement are allowed as the root nodes')
-    })
+  it('should throw if a different type than ExpressionStatement is find in the Program body', () => {
+    expect(expand.bind(null, `
+      const foo = 'bar'
+    `)).toThrowError('Only ExpressionStatement are allowed as the root nodes')
+  })
 
-    it('should split in several ExpressionStatement', () => {
-      expect(expand('a;b').length).toBe(2)
-    })
+  it('should split in several ExpressionStatement', () => {
+    expect(expand('a;b').length).toBe(2)
+  })
 
-    it('should remove comments', () => {
-      expect(expand(`
-        // test
-      `)).toEqual([])
-    })
+  it('should remove comments', () => {
+    expect(expand(`
+      // test
+    `)).toEqual([])
+  })
 
-    it('should expand from Identifier to CallExpression', () => {
-      expect(expand(`
-        a | b
-        a
-        a(b)
-        a.pipe
-      `)).toEqual([
-        'a().pipe(b());',
-        'a();',
-        'a(b());',
-        'a().pipe;',
-      ])
-    })
+  it('should expand from Identifier to CallExpression', () => {
+    expect(expand(`
+      a | b
+      a
+      a(b)
+      a.pipe
+    `)).toEqual([
+      'a().pipe(b());',
+      'a();',
+      'a(b());',
+      'a().pipe;',
+    ])
+  })
 
-    it('should expand from BinaryExpression to .pipe() with left associativity', () => {
-      expect(expand(`
-        a() | b()
-        a() | b() | c()
-      `)).toEqual([
-        'a().pipe(b());',
-        'a().pipe(b()).pipe(c());',
-      ])
-    })
+  it('should expand from BinaryExpression to .pipe() with left associativity', () => {
+    expect(expand(`
+      a() | b()
+      a() | b() | c()
+    `)).toEqual([
+      'a().pipe(b());',
+      'a().pipe(b()).pipe(c());',
+    ])
+  })
 
 })
