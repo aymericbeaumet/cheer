6135ee5b36cf010edc2fb0d8d8fa1f8a23116e75
Aymeric Beaumet <hi@aymericbeaumet.com>
2016-09-16 19:08:55 +0200

    test: cover the shell plugin

---
+++ test/plugins/cheer-plugin-readable-shell.js
@@ -0,0 +1,28 @@
+import { Readable } from 'stream'
+import plugins from '../../src/plugins'
+
+const { shell } = plugins()
+
+describe('shell', () => {
+  it('should be returned by plugins()', () => {
+    expect(shell).toBeTruthy()
+  })
+
+  it('should return an instanceof Readable stream', () => {
+    expect(shell() instanceof Readable).toBe(true)
+  })
+
+  it('should emit the result string', () => {
+    return new Promise((resolve, reject) => {
+      const onData = jest.fn()
+      const readable = shell('echo helloworld')
+      readable
+        .once('error', reject)
+        .once('readable', () => readable.read())
+        .on('data', onData)
+        .once('end', () => resolve([
+          expect(onData.mock.calls).toEqual([ [ 'helloworld\n' ] ])
+        ]))
+    })
+  })
+})
